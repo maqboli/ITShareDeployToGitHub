7bd443c1e1b99d79f9608b6c3e001a42d05ca2ae
MaherMaqboli <maher.maqboli@gmail.com>
2021-03-03 08:46:23 +0000

    first commit

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
